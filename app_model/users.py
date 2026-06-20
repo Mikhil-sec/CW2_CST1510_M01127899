5dcc0b2c5a6c8548b2ba138b72391d5103070401
Mikhil Naika <naikamikhil@gmail.com>
2026-06-20 16:09:39 +0400

    Structure updates

---
+++ app_model/users.py
@@ -0,0 +1,24 @@
+import bycrpt
+
+def New_User():
+    Username = input("Hello, Please enter your username")
+    Password = input("Please enter your password")
+    #add a check if username unique and password strong and verification of password
+    
+    #
+
+
+
+def Login():
+    Username = input("Hello, Please enter your username")
+    Password = input("Please enter your password")
+    #add a check login
+
+def HashPassword_Generator(psw):
+    print()
+
+
+
+def main():
+    print("Main is here")
+
