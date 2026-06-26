f5c65eea5cbcda788da97d39109821aa02d2aa48
Mikhil Naika <naikamikhil@gmail.com>
2026-06-26 18:35:32 +0400

    working streamlit register & login

--- app_model/users.py
+++ app_model/users.py
@@ -18,7 +18,7 @@ def New_User():
 def Login():
     Username = input("Hello, Please enter your username")
     Password = input("Please enter your password")
-    #add a check login
+    #add a check login -- old code for CLI - does it need to exist and work properly?
 
 def Password_Checker(Inputted_Psw,Hash_Psw):
     Byte_Hash_Psw = Hash_Psw.encode('utf-8')
@@ -32,17 +32,42 @@ def HashPassword_Generator(psw):
     hash = bcrypt.hashpw(byte_psw, salt)
     return hash.decode('utf-8')
 
+#=======================================
+#    STREAMLIT REGISTRATION & LOGIN
+#=======================================
+
+
 def Register_User_Streamlit(Username,Password):
     """Takes username/password as arguments instead of asking via input()."""
     conn = get_connection()
 
     existing_user = get_user(conn, Username)
     if existing_user is not None:
-        return False, "Username already exists."
+        return False, "Username already exists." #returns both boolean and string
 
     Hash_Psw = HashPassword_Generator(Password)
     add_user(conn, Username, Hash_Psw)
-    return True, "User successfully registered!"
+    return True, "User successfully registered!" #returns both boolean and string
+
+def Login_User_Streamlit(Username,Password):
+    """Streamlit function for Login"""
+    
+    conn = get_connection()
+    user = get_user(conn, Username)
+    
+    if user is None:
+        
+        return False, "Username does not exist"
+    stored_Hash = user[2]
+    
+    is_valid = Password_Checker(Password,stored_Hash)
+    
+    if is_valid == True:
+        
+        return True, "Login successful!"
+    else:
+        return False, "Incorrect Password"
+    
 
 
 #=====================================
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from app_model.db import get_connection
 from app_model.schema import create_user_table
-from app_model.users import New_User, register_user,login_User
+from app_model.users import New_User, register_user,login_User, Register_User_Streamlit, Login_User_Streamlit
 from app_model.cyber_incidents import migrate_cyber_incidents
 from app_model.it_tickets import migrate_it_tickets
 from app_model.metadatas import migrate_metadatas
+
+import streamlit as st
 
 def Migrate_All_Tables():
     conn = get_connection()
@@ -25,12 +27,67 @@ def cli_authentication_system():
             print('Goodbye!'); 
             break
 
+#========================
+# STREAMLIT FUNCTIONS
+#========================
+
+def Initialise_Session():
+    if "logged_in" not in st.session_state:
+        st.session_state["logged_in"] = False
+    if "username" not in st.session_state:
+        st.session_state["username"] = None
+
+def Dashboard_Login_Register():
+    st.title("Cortex")
+    st.caption("Multi-domain intelligence platform")
+
+    menu = st.sidebar.selectbox("Menu",["Login","Register"])
+    if menu == "Register":
+        st.subheader("Create New Account")
+        username = st.text_input("Username",key="register_username")  #keys used to identify each input box, especially since 2 username used
+        password = st.text_input("Password", type="password",key="register_password") #type = "password" to hide password input on screen
+        if st.button("Register"):
+            success, message = Register_User_Streamlit(username,password)
+            if success:
+                st.session_state["logged_in"]= True
+                st.session_state["username"] = username
+                st.success(message)
+                st.rerun()
+
+            else:
+                st.error(message)
+    elif menu == "Login":
+        st.subheader("Login to your Account")
+        username = st.text_input("Username", key="login_username")
+        password = st.text_input("Password",type="password",key="login_password")
+        if st.button("Login"):
+            success, message = Login_User_Streamlit(username, password)
+            if success:
+                st.session_state["logged_in"] = True
+                st.session_state["username"] = username
+                st.success(message)
+                st.rerun()
+            else:
+                st.error(message)
+
+def User_Dashboard():
+    st.title(f"Welcome, {st.session_state["username"]} !")
+    st.write("This is the dashboard beep bop 😎")
+    if st.button("Logout"):
+        st.session_state["logged_in"] = False
+        st.session_state["username"] = None
+        st.rerun()
+
+def Streamlit_main():
+    if st.session_state["logged_in"]:
+        User_Dashboard()
+    else:
+        Dashboard_Login_Register()
 
 def main():
-
-    print("Welcome")
-    cli_authentication_system()
-
+    Initialise_Session()
+    Streamlit_main()
+    
 if __name__ == "__main__":
     main()
     
